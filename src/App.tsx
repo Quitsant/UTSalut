fa473400947075c63016d708781ab6cb8a479060
Quitsant <ramazhefa@gmail.com>
2026-06-25 13:18:21 +0700

    dashboard selesai

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/header'
 import Footer2 from './components/footer2'
+import Dashboard from './pages/dashboard'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Dashboard />
       <Footer2 />
     </>
   )
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,22 @@
+function Dashboard() {
+    return (
+        
+        <section className="dashboard">
+            <div className="text">
+                <h1>Mau Kerja Sambil Kuliah?</h1>
+                <p className="title">Di Universitas Terbuka Aja!</p>
+                <p className="description">Universitas Terbuka adalah universitas negeri yang menyelenggarakan pendidikan tinggi jarak jauh di Indonesia. Universitas ini didirikan pada 4 September 1984 dan merupakan salah satu perguruan tinggi negeri di Indonesia yang memiliki sistem pembelajaran jarak jauh.</p>
+                <div className="button">
+                    <button><p>Selengkapnya</p></button>
+                </div>
+            </div>
+            <div className="image">
+                <div className="color"></div>
+                <img src="src/assets/imgDash.png" alt="Description" />    
+            </div>
+        </section>
+
+    )
+}
+
+export default Dashboard

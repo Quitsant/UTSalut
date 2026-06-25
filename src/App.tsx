2b37ce6849451b1d7a2446ec11bc59b5779b9380
Quitsant <ramazhefa@gmail.com>
2026-06-25 15:57:52 +0700

    buat feature tabnya and done....

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,68 @@
+import React, { useEffect, useRef, useState } from 'react'
 import './App.css'
 import Header from './components/header'
 import Footer2 from './components/footer2'
 import Dashboard from './pages/dashboard'
+import Feature1 from './pages/feature1'
 
+function ScrollAnimate({ children }: { children: React.ReactNode }) {
+  const [isVisible, setVisible] = useState(false);
+  const domRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setVisible(true);
+        }
+      });
+    }, { threshold: 0.15 }); 
+
+    if (domRef.current) {
+      observer.observe(domRef.current);
+    }
+
+    return () => {
+      if (domRef.current) {
+        observer.unobserve(domRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      ref={domRef}
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+    >
+      {children}
+    </div>
+  );
+}
+
+// --- APP UTAMA ---
 function App() {
+
+  // Auto Reset Webpage ke atas halaman
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual';
+    }
+  }, []);
 
   return (
     <>
       <Header />
-      <Dashboard />
+    
+      <ScrollAnimate>
+        <Dashboard />
+      </ScrollAnimate>
+
+      <ScrollAnimate>
+        <Feature1 />
+      </ScrollAnimate>
+
       <Footer2 />
     </>
   )
---
+++ src/pages/feature1.tsx
@@ -0,0 +1,29 @@
+function feature1() {
+    return (
+        <section className="feature1">
+            <div className="row1">
+                <div className="content">
+                    <div className="text">
+                        <h4>Lorem, ipsum dolor sit amet consectetur.</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus sapiente vero in fugiat aliquam. Sed consequatur adipisci nisi expedita pariatur.</p>
+                    </div>
+                </div> 
+                <img src="src/assets/image.png" />
+            </div>
+
+
+            <div className="row2">
+                <img src="src/assets/image.png" />
+                <div className="content">
+                    <div className="text">
+                        <h4>Lorem, ipsum dolor sit amet consectetur.</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus sapiente vero in fugiat aliquam. Sed consequatur adipisci nisi expedita pariatur.</p>
+                    </div>
+                </div> 
+            </div>
+        </section>
+
+    )
+}
+
+export default feature1
